5711aa2660dbf71f5e679e76fe959e7c632d816a
OHLIA <A520070@QQ.COM>
2017-06-24 15:51:15 +0800

    change hosts link

--- root/1quicklink/myhosts/updatehosts.sh
+++ root/1quicklink/myhosts/updatehosts.sh
@@ -5,9 +5,9 @@ DATE=`date +%Y-%m-%d-%H:%M:%S`
 #HS=`cat /tmp/hosts | grep '127.0.0.1	localhost' | head -1 | cut -d : -f 2`           
 #if [ "${HS}" != "127.0.0.1	localhost" ]                                                       
 #then                                                                            
-  wget -q https://raw.githubusercontent.com/OHLIA/myhosts/master/Go-hosts -O /root/1quicklink/myhosts/1go.hosts --no-check-certificate
+  wget -q https://github.com/OHLIA/myhosts/raw/master/Go-hosts -O /root/1quicklink/myhosts/1go.hosts --no-check-certificate
   wget -q https://raw.githubusercontent.com/vokins/yhosts/master/hosts -O /root/1quicklink/myhosts/2y.hosts --no-check-certificate
-  wget -q https://raw.githubusercontent.com/OHLIA/myhosts/master/other-hosts -O /root/1quicklink/myhosts/3other.hosts --no-check-certificate
+  wget -q https://github.com/OHLIA/myhosts/raw/master/other-hosts -O /root/1quicklink/myhosts/3other.hosts --no-check-certificate
   cat /root/1quicklink/myhosts/*.hosts > /root/1quicklink/myhosts/hosts
   cp /root/1quicklink/myhosts/hosts /tmp/hosts
   killall dnsmasq
